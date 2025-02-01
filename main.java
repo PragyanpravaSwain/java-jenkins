f8ed82343b600688299387bd70eb9805c19878eb
pragyan <pragyanpravaswain@gmail.com>
2025-02-01 16:22:03 +0530

    M

--- main.java
+++ main.java
@@ -3,6 +3,7 @@ import java.util.Date;
 public class Main{
     public static void main(String[] args){
         System.out.println("This is a demo Java Progrm");
+        System.out.println("output");
         System.out.println(new Date());
     }
 }
