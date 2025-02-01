5a18645bf3c16f2cc448bf6b9752a59ac94364fc
pragyan <pragyanpravaswain@gmail.com>
2025-02-01 16:06:05 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("This is a demo Java Progrm");
+        System.out.println(new Date());
+    }
+}
